0ecc37c7fb0b7cda3cd56bcb736c590ee0e0a016
José Lourenço <3s3phinxil@gmail.com>
2025-04-22 13:44:59 -0300

    fix: adjust on livelo partners config url due to 404 error + data examples for easier future validations

--- livelo-partners-analysis/config.ts
+++ livelo-partners-analysis/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     LIVELO: {
-        PARTNERS_CONFIG: "https://www.livelo.com.br/ccstore/v1/files/thirdparty/config_partners_compre_e_pontue.json",
+        PARTNERS_CONFIG: "https://pontos.livelo.com.br/ccstore/v1/files/thirdparty/config_partners_compre_e_pontue.json",
         ACTIVE_PARTNERS_CAMPAIGN: "https://apis.pontoslivelo.com.br/api-bff-partners-parities/v1/parities/active",
     }
 }
---
+++ livelo-partners-analysis/data/format/README.md
@@ -0,0 +1,3 @@
+## Propósito
+
+formatos de dados (com exemplo de parceiros a partir de uma req real) do que é esperado
